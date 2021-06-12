d3bb2be61edcfdd5803cb95bde1168e1814559e0
Guitoh <75733105+Guitoh@users.noreply.github.com>
2021-06-12 17:16:51 -0300

    Add files via upload

---
+++ AnimeManiaScript.lua
@@ -0,0 +1,11 @@
+_G.Level = "Bandit Raid" -- Seleção de mapa ["Leaf Village Gate", "Bandit Raid", "Chunins Exam", "Sand Raid", "Maruto Unlimited Mode", "Hollow Raid", "Corrupt Soul Reaper Ambush", "Vasto Raid", "Arrankar Invasion", "Lord Aiden", "Reaper Unlimited Mode...."]
+_G.Password = "Alguma senha" -- definir senha para w/e, não tenho certeza se várias pessoas usando a mesma senha funcionam
+_G.Distance = "8" -- Distância abaixo durante o autofarm (8 é o padrão, mude como quiser)
+_G.AttackType = "Spin" -- "Spin" irá girar sob eles, o padrão estará apenas abaixo deles.
+_G.AutoMerge = false -- AVISO! ISTO VAI COMBINAR TODOS OS PERSONAGENS NÃO FAVORITADOS/NÃO ESTRELAS. USE POR SUA CONTA E RISCO. TROQUE "false" POR "true" SE VOCÊ QUISER USAR
+_G.MergeCharacter = '' -- Instruções sobre como obter essa chave no video.
+_G.NoSkill1 = false -- Desabilita a habilidade 1 quando estiver 'true'.
+_G.NoSkill2 = false -- Desabilita a habilidade 2 quando estiver 'true'.
+_G.NoSkill3 = false -- Desabilita a habilidade 3 quando estiver 'true'.
+_G.NoSkill4 = false -- Desabilita a habilidade 4 quando estiver 'true'.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Viper123321/anime-mania-autofarm/main/script"))()
